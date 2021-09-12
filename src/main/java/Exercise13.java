5f4f61c97a42dbfc313ccafea6ec90be20e44bb7
Magdalena Sobrino-Almanzar <89550390+magdasobrino@users.noreply.github.com>
2021-09-12 16:13:15 -0400

    Update Exercise13.java

--- src/main/java/Exercise13.java
+++ src/main/java/Exercise13.java
@@ -2,7 +2,7 @@
  *  UCF COP3330 Fall 2021 Assignment 1 Solution
  *  Copyright 2021 Magdalena Sobrino-Almanzar
  */
-package exercise;
+
 
 import java.util.Scanner;
 import java.text.DecimalFormat;
